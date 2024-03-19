1d0a3b630e1e79f51b35e303409dac66b0c4f2e7
Kei Nakano <keinakano415@gmail.com>
2024-03-19 22:40:56 +0900

    refactor example code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [{
+      version: '0.8.20',
+      settings: {
+        evmVersion: 'paris',
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }]
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+  }
 };
 
 export default config;
