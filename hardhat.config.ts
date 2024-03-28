620fe526fd92b5f168560fa10d9a90b1b32cee4c
Kei Nakano <keinakano415@gmail.com>
2024-03-28 19:19:35 +0900

    config: hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,9 +16,31 @@ const config: HardhatUserConfig = {
       }
     }]
   },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        enabled: true,
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
+      }
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2${process.env.ALCHEMY_KEY_SEPOLIA}`,
+      chainId: 11_155_111,
+      accounts: [process.env.PRIVATE_KEY as string]
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
+      chainId: 1,
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  },
   gasReporter: {
     enabled: true,
     currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   }
 };
 
