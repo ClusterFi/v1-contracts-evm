a1603215714195a15c52da4a7a1f465fd1a4cc38
smartcoding51 <keinakano415@gmail.com>
2024-07-25 22:43:16 +0900

    update alchemy api key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,17 +21,17 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         enabled: true,
-        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
         blockNumber: 19879247
       }
     },
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_SEPOLIA}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       chainId: 11_155_111,
       accounts: [process.env.PRIVATE_KEY as string]
     },
     mainnet: {
-      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       chainId: 1,
       accounts: [process.env.PRIVATE_KEY as string]
     }
@@ -42,7 +42,7 @@ const config: HardhatUserConfig = {
   },
   sourcify: {
     enabled: false
-  },  
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   }
